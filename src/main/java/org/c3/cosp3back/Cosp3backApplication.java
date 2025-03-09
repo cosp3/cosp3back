17696acc8453264caa2aad6a084d879f9a624a2a
behem0thik <behem0thik@users.noreply.github.com>
2025-03-10 00:27:38 +0600

    add quick setup

---
+++ src/main/java/org/c3/cosp3back/Cosp3backApplication.java
@@ -0,0 +1,13 @@
+package org.c3.cosp3back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cosp3backApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cosp3backApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/c3/cosp3back/Cosp3backApplicationTests.java
@@ -0,0 +1,13 @@
+package org.c3.cosp3back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cosp3backApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
